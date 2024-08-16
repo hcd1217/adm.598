41276157ec4833f7e2368d6d1fd9b6b72498c1ed
Hậu <hau@spe-labo.com>
2024-08-16 10:52:35 +0700

    sync@20240816.1052

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,47 +1,43 @@
 import { api } from '@/utils/api';
 
-type LoginData = ApiResponse<{
-    token: string;
-}>;
-
 export async function login(form: TLoginForm) {
-    // const response = await api.post<LoginData>(`/auth/login`, form);
-    // return response.data.data;
-    let token = null
-    if(form.email === "viet@mailinator.com" && form.password === "123456@aA") {
-        token = "123456"
-        return Promise.resolve({token})
-    }
-    return Promise.reject({token})
+  // const response = await api.post<LoginData>(`/auth/login`, form);
+  // return response.data.data;
+  let token = null
+  if (form.email === "viet@mailinator.com" && form.password === "123456@aA") {
+    token = "123456"
+    return Promise.resolve({ token })
+  }
+  return Promise.reject({ token })
 }
 
 type RegisterData = ApiResponse<{
-    token: string;
+  token: string;
 }>;
 
 export async function register(form: any) {
-    const response = await api.post<RegisterData>(`/auth/register`, form);
-    return response.data.data;
+  const response = await api.post<RegisterData>(`/auth/register`, form);
+  return response.data.data;
 }
 
 
 export async function identify() {
-    // const response = await api.get<ApiResponse<any>>(`/auth/me`);
-    // return response.data.data;
-    if(Boolean(localStorage.token)) {
-        return Promise.resolve({
-            success: true,
-            statusCode: 200,
-            data: {
-                name: "Viet Ho",
-                email: "viet@mailinator.com",
-                token: "123232"
-            }
-        })
-    }else {
-        return Promise.reject({
-            success: false,
-            statusCode: 400,
-        })
-    }
+  // const response = await api.get<ApiResponse<any>>(`/auth/me`);
+  // return response.data.data;
+  if (Boolean(localStorage.token)) {
+    return Promise.resolve({
+      success: true,
+      statusCode: 200,
+      data: {
+        name: "Viet Ho",
+        email: "viet@mailinator.com",
+        token: "123232"
+      }
+    })
+  } else {
+    return Promise.reject({
+      success: false,
+      statusCode: 400,
+    })
+  }
 }
